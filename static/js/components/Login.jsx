ccb3100a06c8c1ad94b67c123ebfff006abef134
Evelyn Hernández <dark.desires@live.cl>
2016-09-06 16:18:57 -0300

    Adding some examples to see everything works propertly

--- static/js/components/Login.jsx
+++ static/js/components/Login.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import Map from "esri/map";
+import Search from 'esri/dijit/Search';
+import BasemapToggle from "esri/dijit/BasemapToggle";
+import utils from 'esri/arcgis/utils';
 
 class LoginApp extends React.Component {
   constructor(){
@@ -11,13 +14,44 @@ class LoginApp extends React.Component {
     var map = new Map("map_content", { basemap: "topo",
           center: [-122.45, 37.75],
           zoom: 13});
+
+    var search = new Search({
+        map: map
+     }, "search");
+    search.startup();
+
+    var toggle = new BasemapToggle({
+       map: map,
+       basemap: "satellite"
+     }, "BasemapToggle");
+     toggle.startup();
   }
 
   render(){
+    var divStyle1 = {
+      position: 'absolute',
+      right:'20px',
+      top:'10px',
+      'zIndex':'999'
+    }
+    var divStyle2 = {
+      width:'380px',
+      height:'280px',
+      overflow:'auto'
+    }
+
+    var divStyle3 = {
+      padding:'0'
+    }
+
     return (
         <div className="login_wrapper_content">
         <h1>My map</h1>
-          <div id="map_content"></div>
+          <div id="search"></div>
+
+          <div id="map_content">
+            <div id="BasemapToggle"></div>
+          </div>
         </div>
     );
   }
